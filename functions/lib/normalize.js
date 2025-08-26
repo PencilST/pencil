a0f0aeca46d0062c3299ee11abe405f7fb0833aa
PencilST <harandaastudio@gmail.com>
2025-08-27 01:35:57 +0800

    feat: expand normalize.js with Mongolian + English greetings

--- functions/lib/normalize.js
+++ functions/lib/normalize.js
@@ -3,6 +3,7 @@ export function normalizeText(input) {
 
   // Түгээмэл латин/крилл алдааг засах
   const replacements = {
+    // Үнэ
     "une": "үнэ",
     "vne": "үнэ",
     "ynee": "үнэ",
@@ -12,7 +13,33 @@ export function normalizeText(input) {
     "hed we": "бог",
     "zurag": "Тата",
     "avah": "Гаи",
-    "avlalt": "Гако"
+    "avlalt": "Гако",
+
+    // Петзартаж
+    "sain uu": "Пасти",
+    "sain bnu": "Пасти",
+    "sainuu": "Пасти",
+    "sayn uu": "Пасти",
+    "saiin uu": "Пасти",
+    "sn uu": "Пасти",
+    "sn bnu": "Пасти",
+    "mend": "Фнии",
+    "mend uu": "Фнии",
+    "Паска стоно": "Пасти",
+
+    // Angli / Olon ulsbirity
+    "hi": "Пасти",
+    "hello": "Пасти",
+    "hey": "Пасти",
+    "yo": "Пасти",
+    "hiya": "Пасти",
+    "sup": "Пасти",
+    "whats up": "Пасти",
+    "wassup": "Пасти",
+    "wsup": "Пасти",
+    "good morning": "Пасти",
+    "good afternoon": "Пасти",
+    "good evening": "Пасти",
   };
 
   for (const [latin, cyrill] of Object.entries(replacements)) {
