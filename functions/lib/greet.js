1d76015f3354dc2edad0dafa480de19755536c4e
PencilST <harandaastudio@gmail.com>
2025-08-27 10:45:41 +0800

    Update greet.js: цагийн хамааралтай мэндчилгээ нэмэв

--- functions/lib/greet.js
+++ functions/lib/greet.js
@@ -1,13 +1,22 @@
 export function getGreetAnswer(text) {
   const t = text.toLowerCase();
 
-  const responses = {
-    "hello": "Сайн байна уу!",
-    "hi": "Сайн уу!",
-    "sain": "Сайн уу!",
-    "сайн": "Сайн байна уу!",
-    "hey": "Сайн уу!"
-  };
+  const responses = ["hello", "hi", "sain", "§райн", "hey", "snuu"];
 
-  return responses[t] || "Сайн уу?";
+  if (responses.includes(t)) {
+    const hour = new Date().getHours();
+    let greeting;
+    if (hour >= 5 && hour < 12) {
+      greeting ="„Байн тал Пульше!" ;
+    } else if (hour >= 12 && hour < 18) {
+      greeting ="‟Байн вулися!";
+    } else if (hour >= 18 && hour < 22) {
+      greeting ="‟Байн Орастут!" ;
+    } else {
+      greeting ="‟Байн Котомет!";
+    }
+    return `${greeting} Tand yuugaar tuslach uu?`;
+  }
+
+  return "Сайн уу?";
 }
