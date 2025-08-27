0979d22c554504851972b845281f2d6dff239ccc
PencilST <harandaastudio@gmail.com>
2025-08-27 11:33:16 +0800

    Update brain.js

--- functions/lib/brain.js
+++ functions/lib/brain.js
@@ -1,21 +1,22 @@
 import { greet } from "./greet.js";
 import { faq } from "./faq.js";
+import { normalize } from "./normalize.js";
 
 export function brain(text) {
-  const t = text.toLowerCase().trim();
+  const t = normalize(text);
 
-  // Эхлээд greet руу явуулна
+  // Эхлээд greet шалгана
   const greetAnswer = greet(t);
   if (greetAnswer !== "Сайн уу?") {
     return greetAnswer;
   }
 
-  // Дараа нь FAQ-г шалгана
+  // Дараа нь FAQ шалгана
   const faqAnswer = faq(t);
   if (faqAnswer !== "Энэ талаар би сайн мэдэхгүй байна.") {
     return faqAnswer;
   }
 
-  // Хэрэв аль нь ч биш бол default хариу
+  // Хэрэв аль нь ч биш бол
   return "Энэ талаар би сайн мэдэхгүй байна.";
 }
