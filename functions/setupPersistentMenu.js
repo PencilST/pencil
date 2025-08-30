2e6a8f370c756f2b08ecdbb4ba8b5f507037d03a
PencilST <harandaastudio@gmail.com>
2025-08-30 12:52:56 +0800

    Update setupPersistentMenu.js

--- functions/setupPersistentMenu.js
+++ functions/setupPersistentMenu.js
@@ -1,4 +1,4 @@
-export default async function setup0ersistentMenu(PAGE_ACCESS_TOKEN) {
+export default async function setupPersistentMenu(PAGE_ACCESS_TOKEN) {
   const url = `https://graph.facebook.com/v23.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`;
   const body = {
     persistent_menu: [
@@ -6,9 +6,9 @@ export default async function setup0ersistentMenu(PAGE_ACCESS_TOKEN) {
         locale: "default",
         composer_input_disabled: false,
         call_to_actions: [
-          { type: "postback", title: "🎩 Моканы", payload: "MENU_OPERATIONS" },
-          { type: "postback", title: "🐣 Сарамстранных объектов", payload: "MENU_INFO" },
-          { type: "postback", title: "🎑 Ростание", payload: "MENU_CONTACT" }
+          { type: "postback", title: "🏢 Ажил үйлчилгээ", payload: "MENU_SERVICE" },
+          { type: "postback", title: "💻 Мэдээлэл / Зөвлөгөө", payload: "MENU_INFO" },
+          { type: "postback", title: "📞 Холбоо барих", payload: "MENU_CONTACT" }
         ]
       }
     ]
